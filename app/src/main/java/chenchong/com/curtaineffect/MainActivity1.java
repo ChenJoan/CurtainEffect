ab12ef0a46ba0c33989873f2f492cbc6c35c950e
Chong Chen <cchen@ideabinder.com>
2016-03-24 16:43:21 +0800

    commit测试

---
+++ app/src/main/java/chenchong/com/curtaineffect/MainActivity1.java
@@ -0,0 +1,26 @@
+package chenchong.com.curtaineffect;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
